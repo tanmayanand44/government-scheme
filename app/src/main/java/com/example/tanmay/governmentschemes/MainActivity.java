cd19e6aa5cc1d2d117c97503f66fb43da20a4240
tanmayanand44 <anand44tanmay@gmail.com>
2018-03-17 03:34:53 +0530

    yo

--- app/src/main/java/com/example/tanmay/governmentschemes/MainActivity.java
+++ app/src/main/java/com/example/tanmay/governmentschemes/MainActivity.java
@@ -3,19 +3,41 @@ package com.example.tanmay.governmentschemes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText age, housed, category, income;
+    private Button buttonScheme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void addListenerOnButton(){
-        age=(EditText)findViewById(R.id.age);
-        housed=(EditText)findViewbyId(R.id.housed);
-        category=(EditText)findViewbyId(R.id.category);
-        income=(EditText)findViewbyId(R.id.income);
-        buttonScheme=(Button)findViewby
+
+    public void addListenerOnButton() {
+        age = (EditText) findViewById(R.id.age);
+        housed = (EditText) findViewById(R.id.housed);
+        category = (EditText) findViewById(R.id.category);
+        income = (EditText) findViewById(R.id.income);
+        buttonScheme = (Button) findViewById(R.id.button1);
+
+        buttonScheme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value1 = age.getText().toString();
+                String value2 = income.getText().toString();
+                int age1 = Integer.parseInt(value1);
+                int income1 = Integer.parseInt(value2);
+
+            }
+
+        });
+
+
     }
 }
